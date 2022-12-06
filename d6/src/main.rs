0908bd689748d8ef02a825c08413b87dd0a96264
David McGillicuddy <contact@djmcgill.co.uk>
2022-12-06 18:02:09 +0000

    optimise d6

--- d6/src/main.rs
+++ d6/src/main.rs
@@ -5,6 +5,17 @@ use itertools::{self, Itertools};
 
 use std::collections::{hash_map::RandomState, BTreeSet, HashSet};
 
+/*
+test tests::bench_p1_btree     ... bench:     146,111 ns/iter
+test tests::bench_p1_hash      ... bench:     161,523 ns/iter
+test tests::bench_p1_itertools ... bench:     169,472 ns/iter
+test tests::bench_p1_skipping  ... bench:       1,152 ns/iter
+
+test tests::bench_p2_btree     ... bench:     861,028 ns/iter
+test tests::bench_p2_hash      ... bench:     692,197 ns/iter
+test tests::bench_p2_itertools ... bench:     319,863 ns/iter
+test tests::bench_p2_skipping  ... bench:       1,147 ns/iter
+*/
 fn main() {
     let p1 = p_skipping(REAL, 4);
     let p2 = p_skipping(REAL, 14);
@@ -43,17 +54,6 @@ fn p_itertools(s: &str, n: usize) -> usize {
         + n
 }
 
-/*
-test tests::bench_p1_btree     ... bench:     146,111 ns/iter
-test tests::bench_p1_hash      ... bench:     161,523 ns/iter
-test tests::bench_p1_itertools ... bench:     169,472 ns/iter
-test tests::bench_p1_skipping  ... bench:       1,152 ns/iter
-
-test tests::bench_p2_btree     ... bench:     861,028 ns/iter
-test tests::bench_p2_hash      ... bench:     692,197 ns/iter
-test tests::bench_p2_itertools ... bench:     319,863 ns/iter
-test tests::bench_p2_skipping  ... bench:       1,147 ns/iter
-*/
 // in theory to make this nice we'd what need 1) an iterator that does combinations
 // that the j/k loops are doing, and 2) some kind of iterator adapter that lets the inner
 // fn choose how many elements to skip, a la the outer loop
@@ -63,7 +63,9 @@ fn p_skipping(s: &str, n: usize) -> usize {
     'outer: while i + n < s.len() {
         let window = &s[i..i + n];
         // we want to find the furthest most back dupe
-        for j in (0..n).rev() {
+        // I really tried to find something better than just a double for-loop
+        // but everything I tried was 100x slower, wtf
+        for j in (1..n).rev() {
             for k in (0..j).rev() {
                 if window[j] == window[k] {
                     // we found a dupe at indexes (j,k) so we know that
@@ -97,46 +99,57 @@ mod tests {
         assert_eq!(p_skipping(TEST3, 4), 6);
         assert_eq!(p_skipping(TEST4, 4), 10);
         assert_eq!(p_skipping(TEST5, 4), 11);
+
+        // assert_eq!(p_skipping_2(TEST1, 4), 7);
+        // assert_eq!(p_skipping_2(TEST2, 4), 5);
+        // assert_eq!(p_skipping_2(TEST3, 4), 6);
+        // assert_eq!(p_skipping_2(TEST4, 4), 10);
+        // assert_eq!(p_skipping_2(TEST5, 4), 11);
     }
 
-    #[bench]
-    fn bench_p1_btree(b: &mut Bencher) {
-        b.iter(|| p_btree(REAL, 4));
-    }
+    // #[bench]
+    // fn bench_p1_btree(b: &mut Bencher) {
+    //     b.iter(|| p_btree(REAL, 4));
+    // }
 
-    #[bench]
-    fn bench_p2_btree(b: &mut Bencher) {
-        b.iter(|| p_btree(REAL, 14));
-    }
+    // #[bench]
+    // fn bench_p2_btree(b: &mut Bencher) {
+    //     b.iter(|| p_btree(REAL, 14));
+    // }
 
-    #[bench]
-    fn bench_p1_hash(b: &mut Bencher) {
-        b.iter(|| p_hash(REAL, 4));
-    }
+    // #[bench]
+    // fn bench_p1_hash(b: &mut Bencher) {
+    //     b.iter(|| p_hash(REAL, 4));
+    // }
 
-    #[bench]
-    fn bench_p2_hash(b: &mut Bencher) {
-        b.iter(|| p_hash(REAL, 14));
-    }
+    // #[bench]
+    // fn bench_p2_hash(b: &mut Bencher) {
+    //     b.iter(|| p_hash(REAL, 14));
+    // }
 
-    #[bench]
-    fn bench_p1_itertools(b: &mut Bencher) {
-        b.iter(|| p_itertools(REAL, 4));
-    }
+    // #[bench]
+    // fn bench_p1_itertools(b: &mut Bencher) {
+    //     b.iter(|| p_itertools(REAL, 4));
+    // }
 
-    #[bench]
-    fn bench_p2_itertools(b: &mut Bencher) {
-        b.iter(|| p_itertools(REAL, 14));
-    }
+    // #[bench]
+    // fn bench_p2_itertools(b: &mut Bencher) {
+    //     b.iter(|| p_itertools(REAL, 14));
+    // }
 
-    #[bench]
-    fn bench_p1_skipping(b: &mut Bencher) {
-        b.iter(|| p_skipping(REAL, 4));
-    }
+    // #[bench]
+    // fn bench_p1_skipping(b: &mut Bencher) {
+    //     b.iter(|| p_skipping(REAL, 4));
+    // }
 
     #[bench]
     fn bench_p2_skipping(b: &mut Bencher) {
         b.iter(|| p_skipping(REAL, 14));
+    }
+
+    #[bench]
+    fn bench_p2_skipping_2(b: &mut Bencher) {
+        b.iter(|| p_skipping_2(REAL, 14));
     }
 }
 
