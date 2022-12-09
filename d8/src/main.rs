d6bafe9564176e24a72adc2f5078b27c3713216f
David McGillicuddy <contact@djmcgill.co.uk>
2022-12-09 00:37:03 +0000

    finish d8

--- d8/src/main.rs
+++ d8/src/main.rs
@@ -1,4 +1,4 @@
-use std::{cmp::max, collections::HashSet, str::FromStr, time::Instant};
+use std::{cmp::max, time::Instant};
 
 fn main() {
     let start = Instant::now();
@@ -11,6 +11,7 @@ fn main() {
     let mut strictly_descending_from_bot_with_y: Vec<Vec<(usize, i8)>> = vec![vec![]; N];
 
     for (y, row) in INPUT.lines().enumerate() {
+        // todo: don't need to use `i8` with `row.bytes`
         let mut tallest_from_left: i8 = -1;
         let mut strictly_descending_from_right_with_x: Vec<(usize, i8)> = Default::default();
         for (x, tree) in row.bytes().enumerate() {
@@ -36,6 +37,7 @@ fn main() {
             } else {
                 // it might be good, depends if we find a future tree taller or shorter than it
                 // so don't count it yet, and put it in the vec
+                // todo: don't actually care about y
                 strictly_descending_from_bot_with_y[x].push((y, tree));
                 strictly_descending_from_right_with_x.push((x, tree));
             }
@@ -60,10 +62,131 @@ fn main() {
         sum += vec.len();
     }
 
-    let p1 = Instant::now();
-    println!("{}", sum);
+    let p1 = sum;
+    let p1_time = Instant::now();
+
+    // P2
+    // all outer trees are off limits
+    // for an inner tree, for each direction,
+    //     - if the next tree is equal or taller, then dist = 1
+    //     - if the next tree is shorter, then
+    //       we know that dist(this_tree) is at least dist(next_tree)+1
+    //       if _that_ tree is shorter than us, we can skip dist(that_tree) trees too
+    // so we order trees by reverse height, because then we can be sure
+    // we can calculate the answer immediately
+
+    // TODO: ignore edges
+    // TODO: flat vecs?
+    let mut height_grid = vec![vec![0u8; N]; N];
+
+    // for each direction, for each tree, how far can it...see?
+    let mut up_grid = vec![vec![0u8; N]; N];
+    let mut down_grid = vec![vec![0u8; N]; N];
+    let mut left_grid = vec![vec![0u8; N]; N];
+    let mut right_grid = vec![vec![0u8; N]; N];
+
+    // use pqueue?
+    let mut heights = Vec::with_capacity(N * N);
+    for (y, line) in INPUT.lines().enumerate() {
+        if y != 0 && y != N - 1 {
+            for (x, tree) in line.bytes().enumerate() {
+                if x != 0 && x != N - 1 {
+                    height_grid[y][x] = tree;
+                    // todo: do we need to cache height here?
+                    heights.push(((x, y), tree));
+                }
+            }
+        }
+    }
+    heights.sort_by_key(|(_, x)| *x);
+    let mut max_score = 0u32;
+
+    // in theory could parallelise this by doing each direction
+    // independently then would need to work out all the scores
+    // anyway
+    for ((x, y), height) in heights {
+        let up_dist = if y == 1 {
+            1
+        } else {
+            let mut dist = 1u8;
+            while height_grid[y - dist as usize][x] < height {
+                let target_dist = up_grid[y - dist as usize][x];
+                if target_dist == 0 {
+                    break;
+                } // hit edge
+
+                // we can see at least as far as our shorter neighbour can
+                dist += target_dist;
+            }
+            dist
+        };
+        up_grid[y][x] = up_dist;
+
+        let down_dist = if y == N - 2 {
+            1
+        } else {
+            let mut dist = 1u8;
+            while height_grid[y + dist as usize][x] < height {
+                let target_dist = down_grid[y + dist as usize][x];
+                if target_dist == 0 {
+                    break;
+                } // hit edge
+
+                // we can see at least as far as our shorter neighbour can
+                dist += target_dist;
+            }
+            dist
+        };
+        down_grid[y][x] = down_dist;
+
+        let left_dist = if x == 1 {
+            1
+        } else {
+            let mut dist = 1u8;
+            while height_grid[y][x - dist as usize] < height {
+                let target_dist = left_grid[y][x - dist as usize];
+                if target_dist == 0 {
+                    break;
+                } // hit edge
+
+                // we can see at least as far as our shorter neighbour can
+                dist += target_dist;
+            }
+            dist
+        };
+        left_grid[y][x] = left_dist;
+
+        let right_dist = if x == N - 2 {
+            1
+        } else {
+            let mut dist = 1u8;
+            while height_grid[y][x + dist as usize] < height {
+                let target_dist = right_grid[y][x + dist as usize];
+                if target_dist == 0 {
+                    break;
+                } // hit edge
+
+                // we can see at least as far as our shorter neighbour can
+                dist += target_dist;
+            }
+            dist
+        };
+        right_grid[y][x] = right_dist;
+
+        let score =
+            (up_dist as u32) * (down_dist as u32) * (left_dist as u32) * (right_dist as u32);
+
+        if score > max_score {
+            max_score = score;
+        }
+    }
+    let p2 = max_score;
+    let p2_time = Instant::now();
+
+    println!("{} {}", p1, p2);
     // p1: 250.7µs
-    println!("p1: {:?}", p1 - start);
+    println!("p1: {:?}", p1_time - start);
+    println!("p2: {:?}", p2_time - p1_time);
 }
 
 // assumes a strictly descending list
