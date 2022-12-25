b22218f2c65c27b55f4b5ace63d8bde1446544d0
David McGillicuddy <contact@djmcgill.co.uk>
2022-12-25 00:57:20 +0000

    bunch of work on d24

--- d24/src/main.rs
+++ d24/src/main.rs
@@ -6,19 +6,15 @@ use std::{
 
 // const INPUT: &str = TEST;
 const INPUT: &str = REAL;
-type Grid = Vec<Vec<Vec<char>>>;
 
 fn main() {
-    let mut start_time = Instant::now();
+    let start_time = Instant::now();
     let mut grid = vec![];
+    // TODO: can remove the lines() call with more math
     for line in INPUT.lines() {
         let mut row = vec![];
-        for c in line.chars() {
-            if c == '.' {
-                row.push(vec![]);
-            } else {
-                row.push(vec![c]);
-            }
+        for c in line.bytes() {
+            row.push(c);
         }
         grid.push(row);
     }
@@ -31,42 +27,109 @@ fn main() {
     let start_x = grid[0]
         .iter()
         .enumerate()
-        .find(|(_, x)| x.is_empty())
+        .find(|(_, x)| **x == b'.')
         .unwrap()
         .0;
     let end_x = grid[y_len - 1]
         .iter()
         .enumerate()
-        .find(|(_, x)| x.is_empty())
+        .find(|(_, x)| **x == b'.')
         .unwrap()
         .0;
 
-    let lcm = lcm(cycle_h, cycle_w);
-    let grids = prepare_grids(grid, cycle_w, cycle_h, lcm);
-    // for grid in &grids {
-    //     print_grid(grid);
-    // }
+    // precache the valid moves at a given time
+    // todo: snip off the edges tbh
+    let mut okay_x_moves = Vec::with_capacity(cycle_w * (cycle_h + 2) * (cycle_w + 2));
+    for time in 0..cycle_w {
+        for y in 0..cycle_h + 2 {
+            for x in 0..cycle_w + 2 {
+                let okay = if y == 0 || x == 0 || y == cycle_h + 1 || x == cycle_w + 1 {
+                    // edges don't move
+                    grid[y][x] == b'.'
+                } else {
+                    // right moving
+                    let r_candidate_x = (x - 1 + cycle_w - (time % cycle_w)) % cycle_w + 1;
+                    // left moving
+                    let l_candidate_x = (x - 1 + (time % cycle_w)) % cycle_w + 1;
+
+                    grid[y][l_candidate_x] != b'<' && grid[y][r_candidate_x] != b'>'
+                };
+                okay_x_moves.push(okay);
+            }
+        }
+    }
+    let mut okay_y_moves = Vec::with_capacity(cycle_h * (cycle_h + 2) * (cycle_w + 2));
+    for time in 0..cycle_h {
+        for y in 0..cycle_h + 2 {
+            for x in 0..cycle_w + 2 {
+                let okay = if y == 0 || x == 0 || y == cycle_h + 1 || x == cycle_w + 1 {
+                    // edges don't move
+                    grid[y][x] == b'.'
+                } else {
+                    // down moving
+                    let d_candidate_y = (y - 1 + cycle_h - (time % cycle_h)) % cycle_h + 1;
+                    // up moving
+                    let u_candidate_y = (y - 1 + (time % cycle_h)) % cycle_h + 1;
+
+                    grid[d_candidate_y][x] != b'v' && grid[u_candidate_y][x] != b'^'
+                };
+                okay_y_moves.push(okay);
+            }
+        }
+    }
 
     let prep_time = Instant::now();
 
     let neighbours = |&(x, y, time): &(usize, usize, usize)| {
         let time = time + 1;
         let mut neighbours = Vec::with_capacity(5);
-        if is_okay_move(&grids[time % lcm][y][x]) {
+        if is_okay_move(cycle_w, cycle_h, &okay_x_moves, &okay_y_moves, (x, y, time)) {
             neighbours.push(((x, y, time), 1));
         }
-
-        if x > 1 && is_okay_move(&grids[time % lcm][y][x - 1]) {
+        if x > 1
+            && is_okay_move(
+                cycle_w,
+                cycle_h,
+                &okay_x_moves,
+                &okay_y_moves,
+                (x - 1, y, time),
+            )
+        {
             neighbours.push(((x - 1, y, time), 1));
         }
-        if y > 0 && is_okay_move(&grids[time % lcm][y - 1][x]) {
+        // this is the only time we want to move into the top edge
+        if y > 0
+            && is_okay_move(
+                cycle_w,
+                cycle_h,
+                &okay_x_moves,
+                &okay_y_moves,
+                (x, y - 1, time),
+            )
+        {
             neighbours.push(((x, y - 1, time), 1));
         }
-        if x < cycle_w && is_okay_move(&grids[time % lcm][y][x + 1]) {
+        if x < cycle_w
+            && is_okay_move(
+                cycle_w,
+                cycle_h,
+                &okay_x_moves,
+                &okay_y_moves,
+                (x + 1, y, time),
+            )
+        {
             neighbours.push(((x + 1, y, time), 1));
         }
-        // this is the only time we want to move into the edge
-        if y < cycle_h + 1 && is_okay_move(&grids[time % lcm][y + 1][x]) {
+        // this is the only time we want to move into the bottom edge
+        if y < cycle_h + 1
+            && is_okay_move(
+                cycle_w,
+                cycle_h,
+                &okay_x_moves,
+                &okay_y_moves,
+                (x, y + 1, time),
+            )
+        {
             neighbours.push(((x, y + 1, time), 1));
         }
         neighbours
@@ -108,85 +171,20 @@ fn main() {
 }
 
 // we can only move into a space if it's empty
-fn is_okay_move(c: &Vec<char>) -> bool {
-    c.is_empty()
+fn is_okay_move(
+    cycle_w: usize,
+    cycle_h: usize,
+    okay_x_moves: &Vec<bool>,
+    okay_y_moves: &Vec<bool>,
+    (x, y, time): (usize, usize, usize),
+) -> bool {
+    let x_ix = (x, y, time % cycle_w);
+    let y_ix = (x, y, time % cycle_h);
+    okay_x_moves[to_ix(x_ix, cycle_w, cycle_h)] && okay_y_moves[to_ix(y_ix, cycle_w, cycle_h)]
 }
 
-fn prepare_grids(grid: Grid, cycle_w: usize, cycle_h: usize, lcm: usize) -> Vec<Grid> {
-    let mut grids = vec![];
-    grids.push(grid);
-    // could instead split up hori and verti grids
-    for i in 1..lcm {
-        let old_grid = &grids[i - 1];
-        let mut new_grid = old_grid.clone();
-        // print_grid(&new_grid);
-
-        // clear old tornadoes
-        for row in &mut new_grid[1..1 + cycle_h] {
-            for c in &mut row[1..1 + cycle_w] {
-                *c = vec![];
-            }
-        }
-
-        for (y, row) in old_grid[1..1 + cycle_h].iter().enumerate() {
-            let y = y + 1;
-            for (x, cell) in row[1..1 + cycle_w].iter().enumerate() {
-                let x = x + 1;
-                for c in cell {
-                    let (new_x, new_y) = match c {
-                        '>' => {
-                            let new_x = ((x - 1 + 1) % cycle_w) + 1;
-                            (new_x, y)
-                        }
-                        '<' => {
-                            let new_x = ((x - 1 + cycle_w - 1) % cycle_w) + 1;
-                            (new_x, y)
-                        }
-                        'v' => {
-                            let new_y = ((y - 1 + 1) % cycle_h) + 1;
-                            (x, new_y)
-                        }
-                        '^' => {
-                            let new_y = ((y - 1 + cycle_h - 1) % cycle_h) + 1;
-                            (x, new_y)
-                        }
-                        c => unreachable!("{}", c),
-                    };
-                    new_grid[new_y][new_x].push(*c);
-                }
-            }
-        }
-        grids.push(new_grid);
-    }
-    grids
-}
-
-fn gcd(x: usize, y: usize) -> usize {
-    if y == 0 {
-        x
-    } else {
-        gcd(y, x % y)
-    }
-}
-
-fn lcm(x: usize, y: usize) -> usize {
-    x / gcd(x, y) * y
-}
-
-fn print_grid(grid: &Grid) {
-    for row in grid {
-        for cell in row {
-            let s = match cell.len() {
-                0 => '.',
-                1 => cell[0],
-                n => (min(9, n) as u8 + b'0') as char,
-            };
-            print!("{}", s);
-        }
-        println!("");
-    }
-    println!("");
-    println!("");
+fn to_ix((x, y, time): (usize, usize, usize), cycle_w: usize, cycle_h: usize) -> usize {
+    x + y * (cycle_w + 2) + time * (cycle_h + 2) * (cycle_w + 2)
 }
 
 const REAL: &str = include_str!("real.txt");
